6eaaa5d6d1531ba2ff08af8e011c8d67f94aff5f
wuhaoyang-0918 <1648996479@qq.com>
2022-10-19 00:36:38 +0800

    4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("3");
+        System.out.println("4");
         SpringApplication.run(DemoApplication.class, args);
     }
 
